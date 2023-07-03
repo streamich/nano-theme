cf4fbc26d48b9a2e216e9cd92c8c480cf4cfb55c
streamich <streamich@gmail.com>
2023-07-03 14:00:12 +0200

    feat: 🎸 add theming data

---
+++ src/constants.ts
@@ -0,0 +1,33 @@
+export const enum SIZE {
+  SITE_WIDTH = 1300,
+  SITE_PADDING = 32,
+  PAGE_WIDTH = 1000,
+  TOP_NAV_HEIGHT = 64,
+  SIDEBAR_WIDTH = 300,
+}
+
+export const enum ZINDEX {
+  TOP_NAV = 1000,
+  SIDEBAR = 1001,
+  CONTEXT = 1002,
+  PROGRESS = 2000,
+  MODAL = 4000,
+}
+
+export const enum SYMBOL {
+  CHECKMARK = '✓',
+  CROSS = '✗',
+  NIL = '∅',
+  ELLIPSIS = '…',
+  MDASH = '—',
+  NDASH = '–',
+  FDASH = '‒',
+  CTRL = '⌃',
+  CMD = '⌘',
+  ALT = '⌥',
+  SHIFT = '⇧',
+}
+
+export const enum COLOR {
+  LINK = '#0089ff',
+}
